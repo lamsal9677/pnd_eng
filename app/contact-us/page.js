32a4e7487ebd430825c992fdf4d68b77ce1c3699
Sanskar Lamsal <lamsal9677@yahoo.com>
2023-11-22 08:22:04 -0500

    FAQ

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -85,30 +85,107 @@ export default function Home() {
                                     <ul className="accordion-box style-two mb-30">
                                         {/*Accordion Block*/}
                                         <li className="accordion block">
-                                            <div className={isActive.key == 1 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(1)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>01.   What does you do?</div>
+                                            <div className={isActive.key == 1 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(1)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>What services does P&D Engineering Solutions LLC provide?</div>
                                             <div className={isActive.key == 1 ? "acc-content current" : "acc-content"}>
                                                 <div className="content">
-                                                    <div className="text">Same as saying through shrinking from toil and pain these actual teachings of the great explorer of the truth, the master builder of human happiness.
+                                                    <div className="text">We specialize in a range of services, including new construction for single-family residential, alterations and additions, deck design, retaining wall construction, and commercial designs.
                                                     </div>
                                                 </div>
                                             </div>
                                         </li>
                                         {/*Accordion Block*/}
                                         <li className="accordion block">
-                                            <div className={isActive.key == 2 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(2)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>02.  What industries covered?</div>
+                                            <div className={isActive.key == 2 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(2)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>How experienced is your team?</div>
                                             <div className={isActive.key == 2 ? "acc-content current" : "acc-content"}>
                                                 <div className="content">
-                                                    <div className="text">Same as saying through shrinking from toil and pain these actual teachings of the great explorer of the truth, the master builder of human happiness.
+                                                    <div className="text">Our team comprises licensed professional engineers with a combined experience of over 12 years. Each member holds a master's degree in structural engineering, ensuring expertise and proficiency in our field.
                                                     </div>
                                                 </div>
                                             </div>
                                         </li>
                                         {/*Accordion Block*/}
                                         <li className="accordion block">
-                                            <div className={isActive.key == 3 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(3)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>03.  How do you price?</div>
+                                            <div className={isActive.key == 3 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(3)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>In which states is P&D Engineering Solutions LLC licensed to operate?</div>
                                             <div className={isActive.key == 3 ? "acc-content current" : "acc-content"}>
                                                 <div className="content">
-                                                    <div className="text">Same as saying through shrinking from toil and pain these actual teachings of the great explorer of the truth, the master builder of human happiness.
+                                                    <div className="text">Currently, we are licensed in California, Texas, Colorado, Florida, and New Jersey
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        {/* End Block */}
+                                        {/*Accordion Block*/}
+                                        <li className="accordion block">
+                                            <div className={isActive.key == 4 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(4)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>What is the process for initiating a new construction project with your firm?</div>
+                                            <div className={isActive.key == 4 ? "acc-content current" : "acc-content"}>
+                                                <div className="content">
+                                                    <div className="text">The process begins with an initial consultation to understand your needs. From there, we collaborate on the design, obtain necessary permits, and oversee construction to ensure a seamless and successful project.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        {/* End Block */}
+                                        {/*Accordion Block*/}
+                                        <li className="accordion block">
+                                            <div className={isActive.key == 5 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(5)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>Can you handle alterations and additions to existing structures?</div>
+                                            <div className={isActive.key == 5 ? "acc-content current" : "acc-content"}>
+                                                <div className="content">
+                                                    <div className="text">Absolutely. Our team excels in altering and adding to existing structures, carefully preserving their character while incorporating modern functionality.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        {/* End Block */}
+                                        {/*Accordion Block*/}
+                                        <li className="accordion block">
+                                            <div className={isActive.key == 6 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(6)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>What sets P&D Engineering Solutions LLC apart from other engineering firms?</div>
+                                            <div className={isActive.key == 6 ? "acc-content current" : "acc-content"}>
+                                                <div className="content">
+                                                    <div className="text">Our commitment to personalized service, innovative design, and rigorous engineering standards distinguishes us. We prioritize client satisfaction, creative solutions, and the longevity of our projects.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        {/* End Block */}
+                                        {/*Accordion Block*/}
+                                        <li className="accordion block">
+                                            <div className={isActive.key == 7 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(7)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>How do you ensure the sustainability of your designs?</div>
+                                            <div className={isActive.key == 7 ? "acc-content current" : "acc-content"}>
+                                                <div className="content">
+                                                    <div className="text">Sustainability is a key focus. We integrate eco-friendly practices, energy-efficient technologies, and sustainable materials to minimize environmental impact and reduce long-term operational costs.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        {/* End Block */}
+                                        {/*Accordion Block*/}
+                                        <li className="accordion block">
+                                            <div className={isActive.key == 8 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(8)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>What is the typical timeline for completing a residential construction project?</div>
+                                            <div className={isActive.key == 8 ? "acc-content current" : "acc-content"}>
+                                                <div className="content">
+                                                    <div className="text">Timelines vary based on project complexity and scope. During the initial consultation, we provide an estimate and a detailed timeline tailored to your specific project.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        {/* End Block */}
+                                        {/*Accordion Block*/}
+                                        <li className="accordion block">
+                                            <div className={isActive.key == 9 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(9)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>Do you handle commercial design projects, and what types of businesses do you work with?</div>
+                                            <div className={isActive.key == 9 ? "acc-content current" : "acc-content"}>
+                                                <div className="content">
+                                                    <div className="text">Yes, we specialize in commercial designs. Our portfolio includes a diverse range of businesses, from offices and retail spaces to restaurants and more.
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        {/* End Block */}
+                                        {/*Accordion Block*/}
+                                        <li className="accordion block">
+                                            <div className={isActive.key == 10 ? "acc-btn active" : "acc-btn"} onClick={() => handleToggle(10)}><div className="icon-outer"><span className="icon icon_plus flaticon-right" /> <span className="icon icon_minus flaticon-right" /></div>How can I request a consultation or get more information about your services?</div>
+                                            <div className={isActive.key == 10 ? "acc-content current" : "acc-content"}>
+                                                <div className="content">
+                                                    <div className="text">Contact us through our website's "Contact Us" page or call our office to schedule a consultation. We look forward to discussing your project and how we can assist you.
                                                     </div>
                                                 </div>
                                             </div>
