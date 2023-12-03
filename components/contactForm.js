bd541c26e3ec0f28ca2b8dedd9a3b574cbe47a6f
Sanskar Lamsal <lamsal9677@yahoo.com>
2023-12-03 13:23:06 -0500

    forms and deploy ready

--- components/contactForm.js
+++ components/contactForm.js
@@ -3,8 +3,9 @@ import { useEffect } from 'react';
 const CForm = () => {
   useEffect(() => {
     const script = document.createElement("script");
-    script.id = "ff-script";
-    script.src = "https://formfacade.com/include/114781673189337243933/form/1FAIpQLScmhXpVApsO-KUyjEuQahrnyjs5DLPykF3pmqkhveSamBcfGg/classic.js?div=ff-compose";
+    script.id = "ff-script";    
+    script.src = "https://formfacade.com/include/116623072306956651104/form/1FAIpQLScsShGDMy-zIArPqYppXgDHB2IcZ5m--W993FvoQbj72Jz4fA/classic.js?div=ff-compose";
+    
     script.defer = true;
     script.async = true;
     document.body.appendChild(script);
